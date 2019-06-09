f424506dd94a6a5ebb135f0cfe503de993229724
Nonki Takahashi <nobukit@jcom.home.ne.jp>
2019-06-09 21:10:55 +0900

    2019-06-08 Get-TechCommunityArticleList.ps1 v0.4

--- Get-TechCommunityArticleList.ps1
+++ Get-TechCommunityArticleList.ps1
@@ -1,11 +1,12 @@
-﻿# Get-TechCommunityArticleList.ps1 - Get Information from Microsoft Tech Community Blog
-# Version 0.3
+﻿# Get-TechCommunityArticleList.ps1 - Get Article List from Microsoft Tech Community Blog
+# Version 0.4
 # Copyright © 2019 Nonki Takahashi.  The MIT License.
 
 # Usage:
 # .\Get-TechCommunityArticleList [SmallBasic | EducationBlog | AzureDevCommunityBlog [yyyy [q [yyyy [q]]]]]
 
 # History:
+#  0.4  2019-06-08 Changed output from text to object.
 #  0.3  2019-06-07 Renamed to Get-TechCommunityArticleList.
 #  0.2  2019-05-27 Rewrote Get-BlogInfo.
 #  0.1a 2019-05-26 Created as Get-Calendar.
@@ -13,33 +14,36 @@
 # dot source
 . ($PSScriptRoot + '\NetworkLib.ps1')
 
-function Get-BlogInfo {
-    # param $iBlog - index of blog information arrays
+function Get-ArticleInfo {
+    # param $script:iSite - index of site information arrays
     # param $script:ym0 - year and month from
     # param $script:ym1 - year and month to
-    # return $script:blog - blog post array
-    $auth = $a[$iBlog]
-    $long = $l[$iBlog]
-    $blog = $b[$iBlog]
-    $tagB = $t[$iBlog]
-    $url = 'https://techcommunity.microsoft.com/t5/{0}/bg-p/{1}' -f $long, $blog
-    $script:blog = @{}
+    # return article object array
+    $auth = $a[$script:iSite]
+    $long = $l[$script:iSite]
+    $short = $s[$script:iSite]
+    $tagB = $t[$script:iSite]
+    $url = 'https://techcommunity.microsoft.com/t5/{0}/bg-p/{1}' -f $long, $short
     $nArticle = 0
     $maxPage = 1
     $stack = New-Object System.Collections.Stack
     for ($page = 1; $page -le $maxPage; $page++) {
-        $post = @{}
+        $article = @{}
         # get web page contents
-        $url + '/page/' + $page
-        $buf = Get-WebPageContents ($url + '/page/' + $page)
+        if ($page -eq 1) {
+            $pg = ''
+        } else {
+            $pg = '/page/' + $page
+        }
+        $buf = Get-WebPageContents ($url + $pg)
         $script:p = 0
         $tag = 'found'
         while ($tag) {
             # link to a post
             $tag = Find-Tag -tagName 'a' -class 'page-link lia-link-navigation lia-custom-event'
             if ($tag) {
-                $post['url'] = $script:attr['href']
-                $post['title'] = $script:txt.Trim()
+                $article['url'] = $script:attr['href']
+                $article['title'] = $script:txt.Trim()
                 # author and date
                 $tag = Find-Tag -tagName 'div' -class 'author-details'
             }
@@ -50,22 +54,28 @@ function Get-BlogInfo {
                 $script:p = 0
                 $tag = Find-Tag -tagName 'a'
                 if ($tag) {
-                    $post['by'] = $script:txt
+                    if ($auth.ContainsKey($script:txt)) {
+                        $article['by'] = $auth[$script:txt]
+                    } else {
+                        $article['by'] = $script:txt
+                    }
                     $tag = Find-Tag -tagName 'span' 
                 }
                 if ($tag) {
                     $tag = Find-Tag -tagName 'span' 
                 }
                 if ($tag) {
-                    $post['date'] = $script:txt.Substring(0, 10) 
-                    $post['#'] = ++$nArticle
-                    $script:blog += @{$nArticle = $post}
-                    # blog hash
-                    '$post[''#''] = ''{0}''' -f $post['#']
-                    '$post[''title''] = ''{0}''' -f $post['title']
-                    '$post[''url''] = ''{0}''' -f $post['url']
-                    '$post[''by''] = ''{0}''' -f $post['by']
-                    '$post[''date''] = ''{0}''' -f $post['date']
+                    $article['year'] = $script:txt.Substring(6, 4)
+                    $article['month'] = $script:txt.Substring(0, 2)
+                    $article['day'] = $script:txt.Substring(3, 2)
+                    $ym = [int]($article['year'] + $article['month'])
+                    if (($script:ym0 -le $ym) -and ($ym -le $script:ym1)) {
+                        $article['#'] = ++$nArticle
+                        # article
+                        [PSCustomObject]$article
+                    } elseif ($ym -le $script:ym0) {
+                        $tag = ''
+                    }
                 }
                 $script:p = $stack.Pop()
                 $buf = $stack.Pop()
@@ -77,34 +87,36 @@ function Get-BlogInfo {
             $maxPage = $page + 1
         }
     }
-    # /t5/tag/Vijaye%20Raji/tg-p/board-id/SmallBasic
 }
 
-function Initialize-BlogTable {
-    $script:b = @()
+function Initialize-SiteTable {
+    $script:s = @()
     $script:l = @()
     $script:a = @()
     $script:t = @()
     # Small Basic Blog
     $auth = @{}
+    $auth['Ed Price'] = 'Ed'
     $auth['Ed Price - MSFT'] = 'Ed'
     $auth['litdev'] = 'LitDev'
     $auth['Qazwsxedc Qazwsxedc'] = 'Noar Buscher'
+    $auth['NonkiTakahashi']  = 'Nonki'
     $auth['Nonki Takahashi']  = 'Nonki'
-    $script:b += 'SmallBasic'
+    $script:s += 'SmallBasic'
     $script:l += 'Small-Basic-Blog'
     $script:a += $auth
     $script:t += ''
     # Education Blog
-    $script:b += 'EducationBlog'
+    $script:s += 'EducationBlog'
     $script:l += 'Edication-Blog'
     $script:a += @{}
     $script:t += ''
     # Azure Development Community Blog
-    $script:b += 'AzureDevCommunityBlog'
+    $script:s += 'AzureDevCommunityBlog'
     $script:l += 'Azure-Developer-Community-Blog'
     $script:a += @{}
     $script:t += ''
+    # /t5/tag/Vijaye%20Raji/tg-p/board-id/SmallBasic
 }
 
 function Initialize-Cal {
@@ -119,33 +131,37 @@ function Initialize-Cal {
     }
 }
 
-function Test-Arg {
+function Test-Arg ($myArgs) {
+    # param $s - short name array
+    # param $myArgs - arguments array
+    # return $script:site - site short name
     # return $script:yq0 - year and quoter from
     # return $script:yq1 - year and quoter to
     # return $script:ym0 - year and month from
     # return $script:ym1 - year and month to
     # return $script:msg - error message
     # return $script:err - $true if error
-    # return $script:iBlog - blog index
+    # return $script:iSite - site index
     $script:err = $false
-    # check blog
-    if (-not $blog) {
-        $blog = 'SmallBasic'
+    # check site
+    $script:site = $myArgs[0]
+    if (-not $script:site) {
+        $script:site = 'SmallBasic'
     }
-    for ($i = 0; $i -lt $b.Length; $i++) {
-        if ($blog -eq $b[$i]) {
+    for ($i = 0; $i -lt $s.Length; $i++) {
+        if ($script:site -eq $s[$i]) {
             break
         }
     }
-    if ($b.Length -le $i) {
-        $script:msg = 'Usage: .\Get-Calendar {SmallBasic | EducationBlog | AzureDevCommunityBlog} [yyyy [q [yyyy [q]]]]'
+    if ($s.Length -le $i) {
+        $script:msg = 'Usage: .\Get-Calendar [SmallBasic | EducationBlog | AzureDevCommunityBlog [yyyy [q [yyyy [q]]]]]'
         $script:err = $true
     }
-    $script:iBlog = $i
-    if (-not $err) {
+    $script:iSite = $i
+    if (-not $script:err) {
         $today = Get-Date
         # check year from
-        $script:y0 = $script:args[1]
+        $script:y0 = $myArgs[1]
         if (-not $y0) {
             $script:y0 = $today.Year
         }
@@ -156,7 +172,7 @@ function Test-Arg {
     }
     if (-not $err) {
         # check year to
-        $script:y1 = $script:args[3]
+        $script:y1 = $myArgs[3]
         if (-not $y1) {
             $script:y1 = $today.Year
         }
@@ -167,7 +183,7 @@ function Test-Arg {
     }
     if (-not $err) {
         # check quoter from
-        $script:q0 = $script:args[2]
+        $script:q0 = $myArgs[2]
         if (-not $q0) {
             $script:q0 = 1
         }
@@ -178,7 +194,7 @@ function Test-Arg {
     }
     if (-not $err) {
         # check quoter to
-        $script:q1 = $script:args[4]
+        $script:q1 = $myArgs[4]
         if (-not $q1) {
             $script:q1 = 4
         }
@@ -203,18 +219,14 @@ function Test-Arg {
     }
 }
 
-Initialize-BlogTable
-$blog = $args[0]
-Test-Arg
-$url
+Initialize-SiteTable
+"$($args[0]) $($args[1]) $($args[2]) $($args[3]) $($args[4])"
+Test-Arg $args
 if ($err) {
     Write-Host $msg
     return
 }
-"$y0 $q0 $m0"
-"$y1 $q1 $m1"
+"$script:y0 $script:q0 $script:m0 $script:ym0"
+"$script:y1 $script:q1 $script:m1 $script:ym1"
 Initialize-Cal
-$sp[8] + '8'
-$sp[12] + '12'
-$sp[16] + '16'
-Get-BlogInfo
+Get-ArticleInfo
